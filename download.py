4b7197bfdb1f6a79c3e823bdb294e710da0eed1a
gregoryfoster <gregoryfoster@users.noreply.github.com>
2017-10-02 13:42:20 -0700

    Changing filenaming scheme

--- download.py
+++ download.py
@@ -4,11 +4,11 @@ import json
 from pydocket import RegulationDocumentSearch
 
 DOCKET_ID = 'FDA-2017-N-4515'
-OUTPUT_FILENAME = 'docket_comments.json'
+SOURCE_FILENAME = 'docket_comments.json'
 
 print('DOCKET: {}'.format(DOCKET_ID))
 documents = RegulationDocumentSearch.all_comments_by_docket_id(DOCKET_ID)
 
-print("\nWriting output to {}".format(OUTPUT_FILENAME))
-with open(OUTPUT_FILENAME, 'w') as outfile:
+print("\nWriting output to {}".format(SOURCE_FILENAME))
+with open(SOURCE_FILENAME, 'w') as outfile:
     json.dump(documents, outfile, sort_keys=True, separators=(',\n', ': '))
