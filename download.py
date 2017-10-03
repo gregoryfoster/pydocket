5b63a7f3a2f10a033a204ff89c6cb3cef18b6c70
gregoryfoster <gregoryfoster@users.noreply.github.com>
2017-10-02 19:08:06 -0700

    Naming conventionchange

--- download.py
+++ download.py
@@ -4,11 +4,11 @@ import json
 from pydocket import RegulationDocumentSearch
 
 DOCKET_ID = 'FDA-2017-N-4515'
-SOURCE_FILENAME = 'docket_comments.json'
+TARGET_FILENAME = 'docket_comments.json'
 
 print('DOCKET: {}'.format(DOCKET_ID))
 documents = RegulationDocumentSearch.all_comments_by_docket_id(DOCKET_ID)
 
-print("\nWriting output to {}".format(SOURCE_FILENAME))
-with open(SOURCE_FILENAME, 'w') as outfile:
+print('Writing output to {}'.format(TARGET_FILENAME))
+with open(TARGET_FILENAME, 'w') as outfile:
     json.dump(documents, outfile, sort_keys=True, separators=(',\n', ': '))
